1ddbd86e110b476121ae0a197b1f03fccc33a70f
Ariya Hidayat <ariya.hidayat@gmail.com>
2025-08-03 15:12:33 -0700

    Basic server

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello, world!")
+from datetime import datetime
+
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse, HTMLResponse
+from fastapi.routing import APIRoute
 
 
-if __name__ == "__main__":
-    main()
+def custom_generate_unique_id(route: APIRoute) -> str:
+    tag = route.tags[0] if route.tags else "no-tag"
+    return f"{tag}-{route.name}"
+
+
+app = FastAPI(
+    title="Hello FastAPI",
+    version="0.1.0",
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return HTMLResponse(content="<h1>Hello, FastAPI!</h1>", status_code=200)
+
+
+@app.get("/health")
+def health():
+    return PlainTextResponse(content=f"OK {int(datetime.now().timestamp())}")
